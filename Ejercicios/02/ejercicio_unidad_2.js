1e9ba6c1471777647f398bd72e1ea6f5059d3999
roko <ronalc726@gmail.com>
2022-10-12 23:29:44 -0500

    Añadir ejercicio 2

---
+++ Ejercicios/02/ejercicio_unidad_2.js
@@ -0,0 +1,12 @@
+let nombre = Ronald;
+let edad = 20;
+let isDev = true;
+const cumple = new Date(2002, 3, 18);
+const libroFav = {
+    titulo: "El universo elegante",
+    autor: "Brian Greene",
+    anyo: "1999",
+    url: "https://es.wikipedia.org/wiki/El_universo_elegante",
+}
+
+
